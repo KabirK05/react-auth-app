29b37e3511ab789bba197ea621e0a84934a289e1
KabirK05 <77407687+KabirK05@users.noreply.github.com>
2022-11-24 13:21:18 -0800

    Project is Done

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ app.use(fileUpload({
 
 // Routes
 app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
 app.use('/api', require('./routes/upload'))
-
+app.use('/api', require('./routes/productRouter'))
+app.use('/api', require('./routes/paymentRouter'))
 
 // Connect to mongodb
 const URI = process.env.MONGODB_URL
